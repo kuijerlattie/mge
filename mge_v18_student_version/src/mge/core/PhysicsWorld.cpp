f728e06878a254d9bc013ea09aa032de7b0f14fa
Bart "Kuich" Kuijer <kuijerlattie@gmail.com>
2019-04-15 21:24:20 +0200

    beginning of physics

--- mge_v18_student_version/src/mge/core/PhysicsWorld.cpp
+++ mge_v18_student_version/src/mge/core/PhysicsWorld.cpp
@@ -18,5 +18,38 @@ void PhysicsWorld::RegisterCollider(AbstractCollider* pCollider)
 
 void PhysicsWorld::Tick()
 {
+	for (int i = _colliders.size - 1; i >= 0; i--)
+	{
+		for (int j = _colliders.size - 1; j >= 0; j--)
+		{
+			switch (_colliders[i]->type)
+			{
+			case 1:
+				//aabb
+				break;
+			case 2:
+				//obb
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
+bool PhysicsWorld::AABBonAABB(AABBCollider* colliderA, AABBCollider* colliderB)
+{
 
 }
+
+bool PhysicsWorld::AABBonOBB(AABBCollider* colliderA, OBBCollider* colliderB)
+{
+
+}
+
+bool PhysicsWorld::OBBonOBB(OBBCollider* colliderA, OBBCollider* colliderB)
+{
+
+}
+
+
--- mge_v18_student_version/src/mge/core/PhysicsWorld.h
+++ mge_v18_student_version/src/mge/core/PhysicsWorld.h
@@ -14,6 +14,9 @@ public:
 	void Tick();
 private:
 	std::vector<AbstractCollider*> _colliders;
+	bool AABBonAABB(AABBCollider* colliderA, AABBCollider* colliderB);
+	bool AABBonOBB(AABBCollider* colliderA, OBBCollider* colliderB);
+	bool OBBonOBB(OBBCollider* colliderA, OBBCollider* colliderB);
 
 };
 
